e6f92ec74949be21aa8a5d2f83a49017c7140016
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-03-26 00:15:56 +0900

    add controller method.

--- src/main/java/com/example/SampleAPI/controller/SampleAPIController.java
+++ src/main/java/com/example/SampleAPI/controller/SampleAPIController.java
@@ -1,7 +1,11 @@
 package com.example.SampleAPI.controller;
 
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -10,6 +14,22 @@ public class SampleAPIController {
     @RequestMapping(value = "/index")
     public String index() {
         return "hello world";
+    }
+
+    @RequestMapping(value = "/person")
+    public String returnPersonJson() {
+        return "{\"person\" : {\"name\" : \"yamada taro\", \"age\" : 20 }}";
+    }
+
+    @RequestMapping(value = "/person_json")
+    public String returnPersonJson2() {
+        Map<String,String> param = new HashMap<>();
+        param.put("年齢", "20");
+        param.put("性別", "男");
+
+        JSONObject jsonObj = new JSONObject(param);
+
+        return jsonObj.toString();
     }
 
 }
