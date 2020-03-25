e3b29b97295bdb603c657ce0a82e89544b0f51bb
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-03-26 00:03:26 +0900

    add controller method.

--- src/main/java/com/example/SampleAPI/controller/SampleAPI.java
+++ src/main/java/com/example/SampleAPI/controller/SampleAPI.java
@@ -1,4 +1,15 @@
 package com.example.SampleAPI.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
 public class SampleAPI {
+
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "hello world";
+    }
+
 }
