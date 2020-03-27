1da595067c54c84216fa8b99036af5d056bd3717
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-03-27 22:46:41 +0900

    changed tab to space.

--- src/main/java/com/example/SampleAPI/SampleApiApplication.java
+++ src/main/java/com/example/SampleAPI/SampleApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApiApplication.class, args);
+    }
 
 }
