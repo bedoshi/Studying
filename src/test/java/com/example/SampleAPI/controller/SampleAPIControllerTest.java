30c5b9875e34c6db4e0433d83bf51dec531a31aa
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-03-27 22:54:09 +0900

    add test case for using model class.

--- src/test/java/com/example/SampleAPI/controller/SampleAPIControllerTest.java
+++ src/test/java/com/example/SampleAPI/controller/SampleAPIControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.SampleAPI.controller;
 
-import lombok.val;
+import com.example.SampleAPI.model.Person;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +12,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.fail;
@@ -69,6 +72,34 @@ public class SampleAPIControllerTest {
             mockMvc.perform(get("/api/person_json"))
                     .andExpect(status().isOk())
                     .andExpect(content().json(jsonObj.toString()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void canAccessToPersonObjTest() {
+        Person person = new Person();
+        person.setAge(10);
+        person.setName("yamada taro");
+        person.setJob("front-end engineer");
+        person.setSex("male");
+
+        Person father = new Person("yamada jiro", 40, "male", "server side engineer");
+        List<Person> fathersFamily = new ArrayList<>();
+        father.setFamily(fathersFamily);
+        List<Person> family = new ArrayList<>();
+        family.add(father);
+
+        person.setFamily(family);
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            String expectedJson = mapper.writeValueAsString(person);
+            mockMvc.perform(get("/api/person_obj"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json(expectedJson));
         } catch (Exception e) {
             e.printStackTrace();
             fail();
