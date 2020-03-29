4eeb92cfe7fb006c3e1cf9c86f46e56a04cbc6fa
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-03-29 15:35:41 +0900

    add sample test code for service class.

--- src/test/java/com/example/SampleAPI/service/impl/SamplePersonServiceImplTest.java
+++ src/test/java/com/example/SampleAPI/service/impl/SamplePersonServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.example.SampleAPI.service.impl;
 
 import com.example.SampleAPI.controller.response.Person;
+import com.example.SampleAPI.exception.PersonUnder18Exception;
 import com.example.SampleAPI.repository.PersonRepository;
 import com.example.SampleAPI.service.serviceImpl.SamplePersonServiceImpl;
 import org.junit.Assert;
@@ -10,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -22,7 +24,7 @@ public class SamplePersonServiceImplTest {
     SamplePersonServiceImpl samplePersonService;
 
     @Test
-    public void registerPersonDataTest() {
+    public void registerPersonDataWithOver18Test() {
         when(this.personRepository.insertPerson(any())).thenReturn(0);
 
         Person expected = new Person();
@@ -35,5 +37,20 @@ public class SamplePersonServiceImplTest {
         Assert.assertEquals(18, person.getAge());
 
         verify(this.personRepository, times(1)).insertPerson(expected);
+    }
+
+    @Test
+    public void registerPersonDataWithUnder18Test() {
+
+        when(this.personRepository.insertPerson(any())).thenReturn(0);
+
+        try {
+            samplePersonService.registerPersonData("yamada", 17);
+            fail();
+        } catch (PersonUnder18Exception exception) {
+            // do nothing.
+        } catch (Exception e) {
+            fail();
+        }
     }
 }
