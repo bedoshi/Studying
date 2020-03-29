21673292459c617611bd99429c3f5f355a4dec01
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-03-29 15:31:28 +0900

    removed ignorable import.

--- src/main/java/com/example/SampleAPI/controller/rest/SampleServiceCallController.java
+++ src/main/java/com/example/SampleAPI/controller/rest/SampleServiceCallController.java
@@ -2,7 +2,6 @@ package com.example.SampleAPI.controller.rest;
 
 import com.example.SampleAPI.controller.response.Person;
 import com.example.SampleAPI.service.SamplePersonService;
-import com.example.SampleAPI.service.serviceImpl.SamplePersonServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
