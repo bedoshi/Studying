698d0fe31fe0ec08065858d3a46822ef0b56ccf6
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2021-02-03 16:38:18 +0900

    update some dependency version

---
+++ DB/docker/mysql/initdb.d/book.sql
@@ -0,0 +1,6 @@
+create table `book` (
+    book_id int,
+    book_name text,
+    book_isbn text,
+    primary key(book_id)
+);
---
+++ DB/docker/mysql/initdb.d/person.sql
@@ -0,0 +1,6 @@
+create table `person`(
+    `name` varchar(255),
+    `age` int(3),
+    `sex` varchar(10),
+    `job` text,
+);
